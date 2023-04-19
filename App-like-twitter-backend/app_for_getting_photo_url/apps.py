18f2119326c3e5ed3a77105903b3373eb232a470
Łukasz <100160153+LukiOoO@users.noreply.github.com>
2023-04-19 12:31:31 +0200

    project set up

---
+++ App-like-twitter-backend/app_for_getting_photo_url/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppForGettingPhotoUrlConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_for_getting_photo_url'
---
+++ App-like-twitter-backend/pv_messages/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PvMessagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pv_messages'
