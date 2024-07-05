4af98399531b5051a011b7ab02b22a20cd12d77b
LukiOoO <leszczynskilukasz2003@gmail.com>
2024-07-05 16:49:47 +0200

    Add front end project set up

---
+++ app-like-twitter-frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import './globals.css';
+
+export default function Home() {
+  return (
+    <h1 className="">
+      Hello world XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD!
+    </h1>
+  )
+}
